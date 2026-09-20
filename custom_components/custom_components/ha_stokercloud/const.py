d0eaa9723b66b821b9453564a6a0f5d5144977c4
pcmalle <55629266+pcmalle@users.noreply.github.com>
2026-09-20 20:15:35 +0200

    Delete custom_components/custom_components/ha_stokercloud directory

--- custom_components/custom_components/ha_stokercloud/const.py
+++
@@ -1,7 +0,0 @@
-from homeassistant.const import CONF_USERNAME
-import homeassistant.helpers.config_validation as cv
-import voluptuous as vol
-
-DOMAIN = "ha_stokercloud"
-DATA_SCHEMA = vol.Schema({vol.Required(CONF_USERNAME): cv.string})
-PLATFORMS = ['sensor', 'water_heater']
