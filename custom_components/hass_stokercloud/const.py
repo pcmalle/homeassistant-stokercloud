1a607d253a8ed44e8d2aae33c651989cf69f18db
pcmalle <55629266+pcmalle@users.noreply.github.com>
2026-09-20 20:01:17 +0200

    Update const.py

--- custom_components/hass_stokercloud/const.py
+++ custom_components/hass_stokercloud/const.py
@@ -2,6 +2,6 @@ from homeassistant.const import CONF_USERNAME
 import homeassistant.helpers.config_validation as cv
 import voluptuous as vol
 
-DOMAIN = "hass_stokercloud"
+DOMAIN = "ha_stokercloud"
 DATA_SCHEMA = vol.Schema({vol.Required(CONF_USERNAME): cv.string})
 PLATFORMS = ['sensor', 'water_heater']
