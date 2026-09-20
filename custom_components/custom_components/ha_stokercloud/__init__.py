72326acf1114927cfd7502edfd1321af8144415a
pcmalle <55629266+pcmalle@users.noreply.github.com>
2026-09-20 20:10:56 +0200

    Add files via upload

---
+++ custom_components/custom_components/ha_stokercloud/__init__.py
@@ -0,0 +1,41 @@
+"""
+NBE Stoker Cloud
+"""
+
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.typing import ConfigType
+from .const import DOMAIN, PLATFORMS
+from homeassistant.config_entries import ConfigEntry
+import asyncio
+from stokercloud.client import Client as StokerCloudClient
+from homeassistant.const import CONF_USERNAME
+
+
+async def async_setup(hass: HomeAssistant, config: dict):
+    """Set up the component."""
+    hass.data[DOMAIN] = {}
+    return True
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    hass.data[DOMAIN][entry.entry_id] = StokerCloudClient(entry.data[CONF_USERNAME])
+    
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+
+    return True
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Unload a config entry."""
+    unload_ok = all(
+        await asyncio.gather(
+            *[
+                hass.config_entries.async_forward_entry_unload(entry, component)
+                for component in PLATFORMS
+            ]
+        )
+    )
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+
+    return unload_ok
